ce85f9406d4c3b471d09b2a137bd2753fb63cc80
josefinesodling <josefine.sodling@gmail.com>
2017-03-17 15:39:28 +0300

    Revert "max effect button"
    This reverts commit 78d9fcbe31cb9509a0e297f743499b61a90e3b1e.
    
    # Conflicts:
    #	GUI/SimpleOptimizedTabs2.asv
    #	GUI/SimpleOptimizedTabs2.fig
    #	GUI/SimpleOptimizedTabs2.m

---
+++ GUI/.m
@@ -0,0 +1,307 @@
+function varargout = SOT(varargin)
+%{
+    
+    HOW TO CREATE A NEW TAB
+
+    1. Create or copy PANEL and TEXT objects in GUI
+
+    2. Rename tag of PANEL to "tabNPanel" and TEXT for "tabNtext", where N
+    - is a sequential number. 
+    Example: tab3Panel, tab3text, tab4Panel, tab4text etc.
+    
+    3. Add to Tab Code - Settings in m-file of GUI a name of the tab to
+    TabNames variable
+
+    Version: 1.0
+    First created: January 18, 2016
+    Last modified: January 18, 2016
+
+    Author: WFAToolbox (http://wfatoolbox.com)
+
+%}
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @SOT_OpeningFcn, ...
+                   'gui_OutputFcn',  @SOT_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before SOT is made visible.
+function SOT_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to SOT (see VARARGIN)
+
+% Choose default command line output for SOT
+handles.output = hObject;
+
+%% Tabs Code
+% Settings
+TabFontSize = 10;
+TabNames = {'Tab 1','Tab 2','Tab 3'};
+FigWidth = 0.265;
+
+% Figure resize
+set(handles.SimpleOptimizedTab,'Units','normalized')
+pos = get(handles. SimpleOptimizedTab, 'Position');
+set(handles. SimpleOptimizedTab, 'Position', [pos(1) pos(2) FigWidth pos(4)])
+
+% Tabs Execution
+handles = TabsFun(handles,TabFontSize,TabNames);
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes SOT wait for user response (see UIRESUME)
+% uiwait(handles.SimpleOptimizedTab);
+
+% --- TabsFun creates axes and text objects for tabs
+function handles = TabsFun(handles,TabFontSize,TabNames)
+
+% Set the colors indicating a selected/unselected tab
+handles.selectedTabColor=get(handles.tab1Panel,'BackgroundColor');
+handles.unselectedTabColor=handles.selectedTabColor-0.1;
+
+% Create Tabs
+TabsNumber = length(TabNames);
+handles.TabsNumber = TabsNumber;
+TabColor = handles.selectedTabColor;
+for i = 1:TabsNumber
+    n = num2str(i);
+    
+    % Get text objects position
+    set(handles.(['tab',n,'text']),'Units','normalized')
+    pos=get(handles.(['tab',n,'text']),'Position');
+
+    % Create axes with callback function
+    handles.(['a',n]) = axes('Units','normalized',...
+                    'Box','on',...
+                    'XTick',[],...
+                    'YTick',[],...
+                    'Color',TabColor,...
+                    'Position',[pos(1) pos(2) pos(3) pos(4)+0.01],...
+                    'Tag',n,...
+                    'ButtonDownFcn',[mfilename,'(''ClickOnTab'',gcbo,[],guidata(gcbo))']);
+                    
+    % Create text with callback function
+    handles.(['t',n]) = text('String',TabNames{i},...
+                    'Units','normalized',...
+                    'Position',[pos(3),pos(2)/2+pos(4)],...
+                    'HorizontalAlignment','left',...
+                    'VerticalAlignment','middle',...
+                    'Margin',0.001,...
+                    'FontSize',TabFontSize,...
+                    'Backgroundcolor',TabColor,...
+                    'Tag',n,...
+                    'ButtonDownFcn',[mfilename,'(''ClickOnTab'',gcbo,[],guidata(gcbo))']);
+
+    TabColor = handles.unselectedTabColor;
+end
+            
+% Manage panels (place them in the correct position and manage visibilities)
+set(handles.tab1Panel,'Units','normalized')
+pan1pos=get(handles.tab1Panel,'Position');
+set(handles.tab1text,'Visible','off')
+for i = 2:TabsNumber
+    n = num2str(i);
+    set(handles.(['tab',n,'Panel']),'Units','normalized')
+    set(handles.(['tab',n,'Panel']),'Position',pan1pos)
+    set(handles.(['tab',n,'Panel']),'Visible','off')
+    set(handles.(['tab',n,'text']),'Visible','off')
+end
+
+% --- Callback function for clicking on tab
+function ClickOnTab(hObject,~,handles)
+m = str2double(get(hObject,'Tag'));
+
+for i = 1:handles.TabsNumber;
+    n = num2str(i);
+    if i == m
+        set(handles.(['a',n]),'Color',handles.selectedTabColor)
+        set(handles.(['t',n]),'BackgroundColor',handles.selectedTabColor)
+        set(handles.(['tab',n,'Panel']),'Visible','on')
+    else
+        set(handles.(['a',n]),'Color',handles.unselectedTabColor)
+        set(handles.(['t',n]),'BackgroundColor',handles.unselectedTabColor)
+        set(handles.(['tab',n,'Panel']),'Visible','off')
+    end
+end
+
+% --- Outputs from this function are returned to the command line.
+function varargout = SOT_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in togglebutton16.
+function togglebutton16_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton16
+
+
+% --- Executes on button press in radiobutton10.
+function radiobutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton10
+
+
+% --- Executes on button press in radiobutton11.
+function radiobutton11_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton11
+
+
+% --- Executes on button press in radiobutton12.
+function radiobutton12_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton12
+
+
+% --- Executes on button press in radiobutton13.
+function radiobutton13_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton13
+
+
+% --- Executes on button press in radiobutton14.
+function radiobutton14_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton14
+
+
+% --- Executes on button press in radiobutton15.
+function radiobutton15_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton15
+
+
+% --- Executes on button press in radiobutton16.
+function radiobutton16_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton16
+
+
+% --- Executes on button press in radiobutton17.
+function radiobutton17_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton17
+
+
+% --- Executes on button press in togglebutton9.
+function togglebutton9_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton9 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton9
+
+
+% --- Executes on button press in togglebutton10.
+function togglebutton10_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton10
+
+
+% --- Executes on button press in togglebutton11.
+function togglebutton11_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton11
+
+
+% --- Executes on button press in togglebutton12.
+function togglebutton12_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton12
+
+
+% --- Executes on button press in togglebutton13.
+function togglebutton13_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton13
+
+
+% --- Executes on button press in togglebutton14.
+function togglebutton14_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton14
+
+
+% --- Executes on button press in radiobutton18.
+function radiobutton18_Callback(hObject, eventdata, handles)
+% hObject    handle to radiobutton18 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of radiobutton18
+
+
+% --- Executes on button press in togglebutton15.
+function togglebutton15_Callback(hObject, eventdata, handles)
+% hObject    handle to togglebutton15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of togglebutton15
